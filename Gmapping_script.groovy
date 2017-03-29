2d143e328789bc83f8aa4301290c027cc7293b57
sevcon-1 <sevcon.nosql@gmail.com>
2017-03-29 17:43:31 +0100

    Physical Node added
    join logic added but commented out - needs work around defining connector points

--- Gmapping_script.groovy
+++ Gmapping_script.groovy
@@ -14,6 +14,10 @@ import oracle.odi.domain.project.interfaces.DataSet;
 import oracle.odi.domain.model.OdiDataStore;
 import oracle.odi.domain.topology.OdiContext;
 
+import oracle.odi.domain.project.finder.IOdiIKMFinder;
+import oracle.odi.domain.project.finder.IOdiLKMFinder;
+import oracle.odi.domain.project.finder.IOdiCKMFinder;
+
 // Must be Run while attached to repository
 //assert odiInstance.isClosed()
 
@@ -49,13 +53,15 @@ def createExp(DatastoreComponent tgtDSC, OdiDataStore tgtTable, String propertyN
   DatastoreComponent.findAttributeForColumn(tgtDSC,tgtTable.getColumn(propertyName)).setExpressionText(expressionText)
 }
 
+Map srcDsMap = [:]
 mappingFile.eachLine{line ->
     lt = line.tokenize("\t")
     //println "${lt[0]}"
     
     //get mapping line info
-    s = getLineMatch(lt[0], "header")
-    if (r) {
+    //s = getLineMatch(lt[0], "header")
+    //if (r) {
+	if (lt[0] == "header") {
       //println "value of r is ${r}"
       // Extract mapping, project, folder etc and build mapping
       // Check number of elements is OK
@@ -65,13 +71,13 @@ mappingFile.eachLine{line ->
       ff = (IOdiFolderFinder)tme.getFinder(OdiFolder.class)
       
       //println(lt[1])
-      project = pf.findByCode(lt[1])
+	  project = pf.findByCode(lt[1])
       
       //println(lt[2])
       folderColl = ff.findByName(lt[2], lt[1])
       
       //Check only one folder exists
-      assert folderColl.size() == 1 : folderColl.size()
+      assert folderColl.size() == 1// : folderColl.size()
 	  
 	  OdiFolder folder = null
       folder = folderColl.iterator().next()
@@ -81,23 +87,42 @@ mappingFile.eachLine{line ->
       
     }
     
+	
     if (lt[0] == "source") {
       
       srcDs = getDataStore lt[1], lt[2]
       
       //println (srcDs().getName())
-      //objMap["source"] = srcDs
 	  srcDatastoreC  = (DatastoreComponent) map.createComponent("DATASTORE",srcDs(), false);
-      outconn = srcDatastoreC.getOutputPoint()
+      
+	  //Add to a #map for future use
+	  srcDsMap[lt[3]] = srcDatastoreC
+	  outconn = srcDatastoreC.getOutputPoint()
+	  
 	  //bon = srcDatastoreC.getBoundObjectName()
 	  //println("bound object name is: ${bon}")
     }
     
+	if (lt[0] == "join") {
+	  
+	  fromDs = srcDsMap[lt[1]]
+	  toDs = srcDsMap[lt[2]]
+	  joinCond = lt[3]
+	  
+	  println "joining from ${fromDs} to ${toDs}"
+	  
+	  srcJoin = JoinComponent.joinSources("JOIN_DATA", fromDs, toDs, joinCond)
+	  
+	  
+	}
+	
     if (lt[0] == "target") {
       
       tgtDs = getDataStore lt[1], lt[2]
       
-	  tgtDatastoreC  = (DatastoreComponent) map.createComponent("DATASTORE",tgtDs(), false);
+	  tgtDatastoreC  = (DatastoreComponent) map.createComponent("DATASTORE",tgtDs(), false)
+	  tgtDatastoreC.setIntegrationType(lt[4])
+	  
       //println (tgtDs().getName())
       //objMap["source"] = srcDs
 	  
@@ -110,15 +135,38 @@ mappingFile.eachLine{line ->
 	  
     }
 
+    if (lt[0] == "physical") {
+	  
+	  physTgtDesign = map.getPhysicalDesigns()
+      
+	  // Tee up KMs
+	  ikm = ((IOdiIKMFinder)tme.getFinder(OdiIKM.class)).findGlobalByName(lt[1]);
+	  
+	  physTgtDesign.each {
+	      apNodes = it.getAllAPNodes()
+		  tgtNodes = it.getTargetNodes()
+		  apNodes.each { 
+		                 println it.getLKMName()
+		  }
+          tgtNodes.each{
+                        println it.getIKMName()
+						it.setIKM(ikm)
+          }
+	  }
+
+	
+	}
+	
+	
 }
 
 	tm.commit(txnStatus)
 
-println (tgtDs().getName())
-println (srcDs().getName())
+//println (tgtDs().getName())
+//println (srcDs().getName())
 
-//println "${objMap.keySet()}"
-//objMap.each{k, v -> println "${k}->${v}"}
+//println "${srcDsMap.keySet()}"
+//srcDsMap.each{k, v -> println "${k} is the key for ${v}"}
 
 
 
